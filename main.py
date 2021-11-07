8cd4aa8b070afec6c561019fb85f49ea7c65c2b9
Braden <braden.troyer04@gmail.com>
2021-11-07 01:53:20 -0500

    Add vector

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
- 
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -24,28 +30,48 @@ clock = pygame.time.Clock()
 slime = pygame.image.load('resources/Slime_Sprite.png')
 
 
+# North, East, South, West
+direction = {False, False, False, False}
+
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
-                print('left')
+                direction[0] = True
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                print('right')
+                direction[1] = True
             if event.key == pygame.K_UP or event.key == ord('w'):
-                print('up')
+                direction[2] = True
             if event.key == pygame.K_DOWN or event.key == ord('s'):
-                print('down')
+                direction[3] = True
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
-                print('left stop')
+                direction[0] = False
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                print('right stop')
+                direction[1] = False
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                direction[2] = False
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                direction[3] = False
             
         if event.type == pygame.QUIT:
             done = True
+
+    # Set the velocity vector
+    velocity = Vector(0, 0)
+
+    if direction[0]:
+        velocity.y -= 1
+    if direction[1]:
+        velocity.y += 1
+    if direction[2]:
+        velocity.y += 1
+    if direction[3]:
+        velocity.x -= 1
 
 
     # --- Screen-clearing code goes here
